b3b2e7777098601f74c03ab6d0eea9545eecbff3
Chanmoey <relijifa@gmail.com>
2022-07-06 20:55:35 +0800

    启动类。

---
+++ tiny-bilibili-api/src/main/java/com/moon/TinyBilibiliApp.java
@@ -0,0 +1,20 @@
+package com.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author Chanmoey
+ * @date 2022年07月06日
+ */
+
+@SpringBootApplication
+public class TinyBilibiliApp {
+
+    public static void main(String[] args) {
+        ApplicationContext app = SpringApplication.run(
+                TinyBilibiliApp.class, args
+        );
+    }
+}
