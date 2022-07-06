3894df21b4f046bee1f117e3d4466503fa18958f
Chanmoey <relijifa@gmail.com>
2022-07-06 21:50:51 +0800

    Json转换配置。

---
+++ tiny-bilibili-service/src/main/java/com/moon/tinybilibili/service/config/JsonHttpMessageConverterConfig.java
@@ -0,0 +1,35 @@
+package com.moon.tinybilibili.service.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @author Chanmoey
+ * @date 2022年07月06日
+ */
+@Configuration
+public class JsonHttpMessageConverterConfig {
+
+    @Bean
+    @Primary
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH-mm-ss");
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.MapSortField,
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastJsonHttpMessageConverter);
+    }
+}
